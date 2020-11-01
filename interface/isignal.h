9c9a41e7de1b1ee9e8f7182bcc96cd6a23d91026
Norym <myronfranze@web.de>
2020-11-01 11:31:51 +0100

    added signal interfaces for magnetic, acceleration, current, battery(capacity, charge, health)

--- interface/isignal.h
+++ interface/isignal.h
@@ -9,62 +9,103 @@
 namespace siguni::interface
 {
 
-	class ISignal
-	{
-	  public:
-		 virtual ~ISignal() = default;
-		 virtual void UpdateUnit( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignal
+   {
+     public:
+       virtual ~ISignal() = default;
+       virtual void UpdateUnit( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
-	class ISignalGetString
-	{
-	  public:
-		 virtual ~ISignalGetString() = default;
-		 virtual void UpdateUnitSignalGetString( std::string & attKey, std::string & attVersion, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignalGetString
+   {
+     public:
+       virtual ~ISignalGetString() = default;
+       virtual void UpdateUnitSignalGetString( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
-	class ISignalSetReset
-	{
-	  public:
-		 virtual ~ISignalSetReset() = default;
-		 virtual void UpdateUnitSignalSetReset( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignalSetReset
+   {
+     public:
+       virtual ~ISignalSetReset() = default;
+       virtual void UpdateUnitSignalSetReset( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
-	class ISignalGetVoltage
-	{
-	  public:
-		 virtual ~ISignalGetVoltage() = default;
-		 virtual void UpdateUnitSignalGetVoltage( std::string & attKey, std::string & attGetVoltage, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignalGetVoltage
+   {
+     public:
+       virtual ~ISignalGetVoltage() = default;
+       virtual void UpdateUnitSignalGetVoltage( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
-	class ISignalGetVersion
-	{
-	  public:
-		 virtual ~ISignalGetVersion() = default;
-		 virtual void UpdateUnitSignalGetVersion( std::string & attKey, std::string & attVersion, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignalGetVersion
+   {
+     public:
+       virtual ~ISignalGetVersion() = default;
+       virtual void UpdateUnitSignalGetVersion( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
-	class ISignalGetPressure
-	{
-	  public:
-		 virtual ~ISignalGetPressure() = default;
-		 virtual void UpdateUnitSignalGetPressure( std::string & attKey, std::string & attVersion, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignalGetPressure
+   {
+     public:
+       virtual ~ISignalGetPressure() = default;
+       virtual void UpdateUnitSignalGetPressure( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
-	class ISignalGetTemperature
-	{
-	  public:
-		 virtual ~ISignalGetTemperature() = default;
-		 virtual void UpdateUnitSignalGetTemperature( std::string & attKey, std::string & attVersion, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignalGetTemperature
+   {
+     public:
+       virtual ~ISignalGetTemperature() = default;
+       virtual void UpdateUnitSignalGetTemperature( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
-	class ISignalGetHumidity
-	{
-	  public:
-		 virtual ~ISignalGetHumidity() = default;
-		 virtual void UpdateUnitSignalGetHumidity( std::string & attKey, std::string & attVersion, CAdditionals & attAdditionals ) = 0;
-	};
+   class ISignalGetHumidity
+   {
+     public:
+       virtual ~ISignalGetHumidity() = default;
+       virtual void UpdateUnitSignalGetHumidity( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
+   class ISignalGetMagnetic
+   {
+     public:
+       virtual ~ISignalGetMagnetic() = default;
+       virtual void UpdateUnitSignalGetMagnetic( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
+
+   class ISignalGetAcceleration
+   {
+     public:
+       virtual ~ISignalGetAcceleration() = default;
+       virtual void UpdateUnitSignalGetAcceleration( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
+
+   class ISignalGetCurrent
+   {
+     public:
+       virtual ~ISignalGetCurrent() = default;
+       virtual void UpdateUnitSignalGetCurrent( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
+
+   class ISignalGetBatteryCapacity
+   {
+     public:
+       virtual ~ISignalGetBatteryCapacity() = default;
+       virtual void UpdateUnitSignalGetBatteryCapacity( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
+
+   class ISignalGetBatteryCharge
+   {
+     public:
+       virtual ~ISignalGetBatteryCharge() = default;
+       virtual void UpdateUnitSignalGetBatteryCharge( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
+
+   class ISignalGetBatteryHealth
+   {
+     public:
+       virtual ~ISignalGetBatteryHealth() = default;
+       virtual void UpdateUnitSignalGetBatteryHealth( std::string & attKey, std::string & attValue, CAdditionals & attAdditionals ) = 0;
+   };
 
 }
 
